339ff2cfa5358e245f7465814978960207a16ab8
Norapat Buppodom <norapat@box.norapat.com>
2019-01-01 20:04:59 +0700

    test

--- source.js
+++ source.js
@@ -1,3 +1,6 @@
+let debug = document.getElementById('debug');
+debug.textContent = "";
+
 class Observable {
     constructor() {
         this.handlers = [];
@@ -24,13 +27,20 @@ let arrayOfTextElement = document.querySelectorAll(".text");
 
 function update(text) {
     this.innerHTML = text;
+    
 }
 // const updateT1 = text => this.innerHTML = text;
 // const updateT2 = text => t2.innerHTML = text;
 // const updateT3 = text => t3.innerHTML = text;
 
 let observer = new Observable();
-arrayOfTextElement.forEach(element => observer.subscribe(update.bind(element)));
+arrayOfTextElement.forEach((element, index) => {
+// 	observer.subscribe(update.bind(arrayOfTextElement[index]));
+// 	debug.textContent = update.bind().toString()
+// 	debug.textContent += element == arrayOfTextElement[index]; //true
+// 	debug.textContent += update.bind(element) == update.bind(arrayOfTextElement[index]); //false
+// 	debug.textContent += update == update;
+});
 // observer.subscribe(updateT1);
 // observer.subscribe(updateT2);
 // observer.subscribe(updateT3);
@@ -42,10 +52,19 @@ let btnsUnsubscribe = document.querySelectorAll(".unsubscribe");
 
 // please fix subscribe button
 btnsSubscribe.forEach((element, index) => {
-    element.addEventListener('click', e => observer.subscribe(update.bind(arrayOfTextElememt[index])));
+    element.addEventListener('click', e => { 
+    	observer.subscribe(update.bind(arrayOfTextElememt[index]));
+    	debug.textContent += "3";
+    });
 });
 
 // please add unsubscribe button
 btnsUnsubscribe.forEach((element, index) => {
-    element.addEventListener('click', e => observer.unsubscribe(update.bind(arrayOfTextElement[index])));
+// 	debug.textContent += index;
+    element.addEventListener('click', e => {
+    	observer.unsubscribe(update.bind(arrayOfTextElement[index]));
+    	debug.textContent += "gug"
+    });
 });
+
+// debug.textContent = btnsSubscribe.toString(); o
