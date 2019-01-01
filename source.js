59c7b0878f3376211ce3accd46e55eaaeabb140e
new5558 <new17353@gmail.com>
2019-01-01 13:19:48 +0700

    loop commit

--- source.js
+++ source.js
@@ -17,23 +17,32 @@ class Observable {
 }
 
 let textbox = document.getElementById('textbox');
-let t1 = document.getElementById('t1');
-let t2 = document.getElementById('t2');
-let t3 = document.getElementById('t3');
+// let t1 = document.getElementById('t1');
+// let t2 = document.getElementById('t2');
+// let t3 = document.getElementById('t3');
+let arrayOfTextElement = document.querySelectorAll(".text");
 
-const updateT1 = text => t1.innerHTML = text;
-const updateT2 = text => t2.innerHTML = text;
-const updateT3 = text => t3.innerHTML = text;
+function update(text) {
+    this.innerHTML = text;
+}
+// const updateT1 = text => this.innerHTML = text;
+// const updateT2 = text => t2.innerHTML = text;
+// const updateT3 = text => t3.innerHTML = text;
 
 let observer = new Observable();
-observer.subscribe(updateT1);
-observer.subscribe(updateT2);
-observer.subscribe(updateT3);
+arrayOfTextElement.forEach(element => observer.subscribe(update.bind(element)));
+// observer.subscribe(updateT1);
+// observer.subscribe(updateT2);
+// observer.subscribe(updateT3);
 
 textbox.addEventListener('keyup', e => observer.notify(e.target.value));
 
 let btnsSubscribe = document.querySelectorAll(".subscribe");
 let btnsUnsubscribe = document.querySelectorAll(".unsubscribe");
-btn1.addEventListener('click', e => {
-    observer.unsubscribe(updateT1)
-});
+
+// please fix subscribe button
+// btnsSubscribe.forEach(element => {
+    // element.addEventListener('click', e => observer.subscribe());
+// });
+
+// please add unsubscribe buttin
